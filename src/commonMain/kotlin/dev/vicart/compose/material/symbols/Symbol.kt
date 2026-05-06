5fde740e3f5f57bd154206fee6f5753c194eb6fb
Clement Vicart <clem.vicart@gmail.com>
2026-05-06 19:23:19 +0200

    fix: Updated Material Symbols font files & fixed icon size

--- src/commonMain/kotlin/dev/vicart/compose/material/symbols/Symbol.kt
+++ src/commonMain/kotlin/dev/vicart/compose/material/symbols/Symbol.kt
@@ -1,10 +1,8 @@
 package dev.vicart.compose.material.symbols
 
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
